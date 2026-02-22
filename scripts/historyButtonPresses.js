a4a9ffdc6d246748b897c765d7b13bd3257ca2d9
A10Wartpog <albertcioara01@gmail.com>
2026-02-23 01:46:00 +0200

    Progress

--- scripts/historyButtonPresses.js
+++ scripts/historyButtonPresses.js
@@ -1,32 +1,39 @@
 function changeText(year) {
     const textElement = document.querySelector('.history-description')
     const buttonText = document.querySelector('.button' + year)
-    const allButtons = document.querySelectorAll('.year-button')
-    console.log(toString(allButtons))
-    allButtons.forEach((button) => {
-        button.style.textDecoration = 'none'
-    })
-
+    removeUnderline()
     switch (year) {
         case 2022:
             textElement.innerHTML = 'text 2022'
             buttonText.style.textDecoration = 'underline'
+            buttonText.classList.add('is-toggled')
             break
         case 2023:
             textElement.innerHTML = 'text 2023'
             buttonText.style.textDecoration = 'underline'
+            buttonText.classList.add('is-toggled')
             break
         case 2024:
             textElement.innerHTML = 'text 2024'
             buttonText.style.textDecoration = 'underline'
+            buttonText.classList.add('is-toggled')
             break
         case 2025:
             textElement.innerHTML = 'text 2025'
             buttonText.style.textDecoration = 'underline'
+            buttonText.classList.add('is-toggled')
             break
         case 2026:
             textElement.innerHTML = 'text 2026'
             buttonText.style.textDecoration = 'underline'
+            buttonText.classList.add('is-toggled')
             break
+    }
+}
+function removeUnderline() {
+    const underlinedText = document.querySelector('.is-toggled')
+    if (underlinedText) {
+        underlinedText.classList.remove('is-toggled')
+        underlinedText.style.textDecoration = 'none'
     }
 }
